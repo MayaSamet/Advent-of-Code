acf54f51bc5355250e99d5e1f36665e7e8758526
Maya Samet <mayasamet@gmail.com>
2020-02-21 21:25:33 -0800

    begun day 1 pt 2 - midway thru converting to individual module sums

--- day1/elf_fuel_counter.py
+++ day1/elf_fuel_counter.py
@@ -1,11 +1,26 @@
+# part 1
 f = open('input.txt', 'r')
 masses = f.readlines()
 masses = list(map(int, masses)) 
 f.close()
 
-total_fuel = int(0)
+total_fuel = 0
 
 for mass in masses:
     total_fuel += int(mass/3)-2
 
-print(total_fuel)
+# for solution:
+# print(total_fuel)
+
+# part 2
+
+total_fuel_2 = 0
+ind = 0
+
+for mass in masses:
+    
+    fuel_mass = mass
+    while fuel_mass > 0:
+        fuel_mass = int(fuel_mass/3)-2
+        total_fuel_2 += fuel_mass
+        
